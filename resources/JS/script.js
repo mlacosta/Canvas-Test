9fc83deff0f877983e0da0b48183af1e59e2180a
mlacosta <marianoacosta.003@gmail.com>
2020-06-23 16:27:27 -0300

    implementing mechanics

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -1,7 +1,9 @@
 class Paddle {
     constructor(gameWidth, gameHeight){
         this.width = 150;
-        this.height
+        this.height = 20;
+        this.maxSpeed = 7;
+        this.speed = 0;
 
         this.position = {
             x: gameWidth/2 - this.width/2,
@@ -9,8 +11,35 @@ class Paddle {
         }
     }
 
-    draw(context) {
-        context.fillRect(this.position.x,this.position.y,this.width,this.height);
+    draw(ctx) {
+        ctx.fillStyle = '#5bf';
+        ctx.fillRect(this.position.x,this.position.y,this.width,this.height);
+    }
+
+    update(dt){
+        //dt = delta time
+        if(!dt) return;
+        this.position.x += this.speed;
+    }
+
+    moveLeft(){
+        this.speed = -this.maxSpeed;
+    }
+}
+
+class InputHandler {
+    constructor(paddle){
+        document.addEventListener('keydown', (event)=>{
+
+            switch(event.keyCode){
+                case 37:
+                    paddle.moveLeft();
+                    break;
+                case 39:
+                    alert('move right');
+                    break;
+            }
+        })
     }
 }
 
@@ -20,8 +49,21 @@ let context = canvas.getContext('2d'); //rendering context to draw into the canv
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-context.clearRect(0,0,800,600) //from start to the entire game screen
 
 let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT) 
 
-paddle.draw(context)
+let lastTime = 0;
+
+const gameLoop = (timeStamp)=>{
+    let dt = timeStamp - lastTime;
+    lastTime = timeStamp;
+
+    context.clearRect(0,0,800,600) //from start to the entire game screen
+    paddle.update(dt);
+    paddle.draw(context);
+
+    requestAnimationFrame(gameLoop)
+    
+}
+new InputHandler(paddle);
+gameLoop()
